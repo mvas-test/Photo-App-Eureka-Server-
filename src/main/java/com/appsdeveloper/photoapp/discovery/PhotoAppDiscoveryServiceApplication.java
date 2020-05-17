a6221248d02d77f83f8dcff76164919daec2d36f
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2020-05-17 16:18:13 +0100

    First Commit

---
+++ src/main/java/com/appsdeveloper/photoapp/discovery/PhotoAppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloper.photoapp.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
+	}
+
+}
